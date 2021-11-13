f62d5875ad8f121dc6f796640d70e5df3cc1e7fb
Kirk <robotics@northwoodschool.com>
2021-11-12 22:35:17 -0500

    replaced shooter Talons with SparkMax+Neo

--- src/main/java/frc/robot/subsystems/RonMK1.java
+++ src/main/java/frc/robot/subsystems/RonMK1.java
@@ -6,13 +6,19 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 
 public class RonMK1 extends SubsystemBase{  
     private static RonMK1 m_instance;
-    TalonSRX _talon = new TalonSRX(20);
-    TalonSRX _talon2 = new TalonSRX(21);
+    //TalonSRX _talon = new TalonSRX(20);
+   // TalonSRX _talon2 = new TalonSRX(21);
+
+    CANSparkMax _Shooter1 = new CANSparkMax(20, MotorType.kBrushless);
+    CANSparkMax _Shooter2 = new CANSparkMax(21, MotorType.kBrushless);
 
 
 
@@ -37,19 +43,20 @@ public class RonMK1 extends SubsystemBase{
 
 
     public void FireRon(double percent){
-        _talon.set(ControlMode.PercentOutput, percent);
-        _talon2.set(ControlMode.PercentOutput,-percent);
+        _Shooter1.set(percent);
+        _Shooter2.set(-percent);
       
     }
     public void setBrakeMode(boolean mode){
         if (mode == true){
-            _talon.setNeutralMode(NeutralMode.Brake);
-            _talon2.setNeutralMode(NeutralMode.Brake);
+            _Shooter1.setIdleMode(IdleMode.kBrake);
+            _Shooter2.setIdleMode(IdleMode.kBrake);
+            //_talon2.setNeutralMode(NeutralMode.Brake);
            
         }
         else{
-            _talon.setNeutralMode(NeutralMode.Coast);
-            _talon2.setNeutralMode(NeutralMode.Coast);
+            _Shooter1.setIdleMode(IdleMode.kCoast);
+            _Shooter2.setIdleMode(IdleMode.kCoast);
         ;
 
         }
