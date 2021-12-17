e7214d4472f45c039cf57783570451d177e29ac5
Kirk <robotics@northwoodschool.com>
2021-12-17 15:44:09 -0500

    Yeet

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,17 +2,52 @@ package frc.robot.subsystems;
 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import javax.annotation.concurrent.GuardedBy;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Intake extends SubsystemBase{
     private static Intake m_instance;
     TalonSRX _talon3 = new TalonSRX(22);
+    Solenoid extendIntake = new Solenoid(1);
+
+    private double IntakePercent = 0.0;
+
+    private final Object stateLock = new Object(); 
+    @GuardedBy("intakeState")
+    boolean IntakeExtended = false;
 
     public Intake(){
         super();
         setBrakeMode(true);
+    }
+
+    public void update(double time){
+        double localMotorOutput;
+        boolean localIntakeState; 
+        synchronized (stateLock){
+            localMotorOutput = IntakePercent;
+            localIntakeState = IntakeExtended;
+    
+    
+
+            }
+            _talon3.set(ControlMode.PercentOutput, localMotorOutput);
+    
+            if (localIntakeState != extendIntake.get()){
+                extendIntake.set(localIntakeState);
+
+        }
+
+
+
+ 
+
     }
 
 
@@ -24,6 +59,20 @@ public class Intake extends SubsystemBase{
         return m_instance;
     }
 
+    public void ExtendIntake(boolean IntakeState){
+
+        synchronized (stateLock){
+            this.IntakeExtended = IntakeState;
+        }
+    }
+
+    public boolean isIntakeExtended(){
+        synchronized (stateLock){
+            return IntakeExtended;
+        }
+
+
+    }
     public void RunIntake(double percent){
         _talon3.set(ControlMode.PercentOutput, percent);
     }
--- src/main/java/frc/robot/swerve/SwerveDrive.java
+++ src/main/java/frc/robot/swerve/SwerveDrive.java
@@ -1,6 +1,7 @@
 package frc.robot.swerve;
 
 import edu.wpi.first.wpilibj.GyroBase;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Units;
 
@@ -11,7 +12,7 @@ public class SwerveDrive {
     private SwerveWheel leftBackWheel;
     private SwerveWheel rightBackWheel;
 
-    private GyroBase gyro;
+    private AHRS gyro;
 
     private double wheelbase = Units.inchesToMeters(28);
     private double trackwidth = Units.inchesToMeters(28);
@@ -22,7 +23,7 @@ public class SwerveDrive {
     private double slowModeSpeedMultiplier = 0.45;
 
     public SwerveDrive(SwerveWheel rightFront, SwerveWheel leftFront, SwerveWheel leftBack, SwerveWheel rightBack,
-            GyroBase gyro) {
+            AHRS gyro) {
         this.rightFrontWheel = rightFront;
         this.leftFrontWheel = leftFront;
         this.leftBackWheel = leftBack;
